4fa4c6937d25a5e4ff107e503d1a05c3521cff41
Erica <ewalsh@yieldstreet.com>
2020-08-29 16:17:03 -0400

    Updates

---
+++ precache-manifest.fd8c71dc794927493403dc1200a4a57c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c8c76d95a147146c308181947ffc6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db890f8c6e6203ba4c7",
+    "url": "/static/js/2.918d3e05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.918d3e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c70cf17241dac3aa6e",
+    "url": "/static/js/main.acaceb84.chunk.js"
+  },
+  {
+    "revision": "3a6fc275ee79597b8cd8",
+    "url": "/static/js/runtime-main.8f85031c.js"
+  }
+]);
